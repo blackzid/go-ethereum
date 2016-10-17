c86b151df350e1d27e5335dd8d26ba1ff55ccc4b
blackzid <blackzidd@gmail.com>
2016-10-17 11:37:33 +0800

    /eth/hdcProtocol.go: update

--- eth/hdcProtocol.go
+++ eth/hdcProtocol.go
@@ -28,15 +28,15 @@ const (
 
 // eth protocol message codes
 const (
-	// Protocol messages belonging to eth/62
-	StatusMsg            = 0x00
-	TxMsg                = 0x01
-	GetBlockProposalsMsg = 0x02
-	BlockProposalsMsg    = 0x03
-	NewBlockProposalMsg  = 0x04
-	VotingInstructionMsg = 0x05
-	VoteMsg              = 0x06
-	ReadyMsg             = 0x07
+	// Protocol messages belonging to hdc
+	HDCStatusMsg            = 0x10
+	TxMsg                = 0x11
+	GetBlockProposalsMsg = 0x12
+	BlockProposalsMsg    = 0x13
+	NewBlockProposalMsg  = 0x14
+	VotingInstructionMsg = 0x15
+	VoteMsg              = 0x16
+	ReadyMsg             = 0x17
 )
 
 type errCode int
@@ -68,4 +68,32 @@ var errorToString = map[int]string{
 	ErrNoStatusMsg:             "No status message",
 	ErrExtraStatusMsg:          "Extra status message",
 	ErrSuspendedPeer:           "Suspended peer",
+}
+type HDCStatusMsg struct {
+	ProtocolVersion uint32
+	NetworkId       uint32
+	TD              *big.Int
+	CurrentLockset  *LockSet
+	GenesisBlock    common.Hash
+}
+// Requests a BlockProposals message detailing a number of blocks to be sent, each referred to
+// by block number. Note: Don't expect that the peer necessarily give you all these blocks
+// in a single message - you might have to re-request them.
+type getBlockProposals struct {
+	Number uint64      // Number of one particular block being announced
+}
+type blockProposalsData struct {
+	BlockProposals []*types.BlockProposal
+}
+type newBlockProposals struct {
+	BlockProposal *types.BlockProposal
+}
+type votingInstructionData struct {
+	VotingInstruction *types.VotingInstruction
+}
+type voteData struct {
+	Vote *types.Vote
+}
+type readyData struct {
+	Ready *types.Ready
 }
